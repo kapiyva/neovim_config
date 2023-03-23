f7928920bcc16c1be30fb918318ec7e5aa77b327
Kapiyva <ri9.py1201@gmail.com>
2023-03-24 02:18:06 +0900

    sort require

--- init.lua
+++ init.lua
@@ -7,10 +7,10 @@ vim.api.nvim_create_user_command("PackerCompile", [[packadd packer.nvim | lua re
 require('options')
 
 require('config_plugin.coc')
-require('config_plugin.telescope')
+require('config_plugin.fern')
+require('config_plugin.gitsigns')
 require('config_plugin.lualine')
 require('config_plugin.nord')
 require('config_plugin.scrollbar')
-require('config_plugin.fern')
-require('config_plugin.gitsigns')
+require('config_plugin.telescope')
 
