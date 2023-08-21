b9dd27f6b256b60904edbc190d02e7e55765f4a0
kapiyva <ri9.py1201@gmail.com>
2023-08-22 03:10:09 +0900

    add telescope command

--- lua/pconf/telescope.lua
+++ lua/pconf/telescope.lua
@@ -12,6 +12,7 @@ require('telescope').setup({
 require('telescope').load_extension('coc')
 
 local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>fa', "<cmd>Telescope<cr>", {})
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
