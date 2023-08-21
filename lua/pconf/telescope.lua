ed14bf1236aa7934d69c620829b2c9898258df99
kapiyva <ri9.py1201@gmail.com>
2023-08-22 02:48:49 +0900

    add coc plugin to telescope

--- lua/pconf/telescope.lua
+++ lua/pconf/telescope.lua
@@ -1,11 +1,20 @@
-require("telescope").setup({
+require('telescope').setup({
   defaults = {
-    file_ignore_patterns = { "node_modules", ".git", "venv", },
+    file_ignore_patterns = { 'node_modules', '.git', 'venv', },
   },
+  extentions = {
+    coc = {
+      prefer_locations = true,
+    }
+  }
 })
 
-local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+require('telescope').load_extension('coc')
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>fd', "<cmd>Telescope coc workspace_diagnostics<cr>", {})
+
