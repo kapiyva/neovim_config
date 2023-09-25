3f233f774a6bbfc629bf236cc4d63eaa55f536c3
kapiyva <ri9.py1201@gmail.com>
2023-09-25 23:48:58 +0900

    fix typo

--- lua/pconf/gitsigns.lua
+++ lua/pconf/gitsigns.lua
@@ -9,14 +9,14 @@ require('gitsigns').setup{
     end
 
     -- Navigation
-    map('n', 'g<', function()
-      if vim.wo.diff then return 'g<' end
+    map('n', 'g>', function()
+      if vim.wo.diff then return 'g>' end
       vim.schedule(function() gs.next_hunk() end)
       return '<Ignore>'
     end, {expr=true})
 
-    map('n', 'g>', function()
-      if vim.wo.diff then return 'g>' end
+    map('n', 'g<', function()
+      if vim.wo.diff then return 'g<' end
       vim.schedule(function() gs.prev_hunk() end)
       return '<Ignore>'
     end, {expr=true})
