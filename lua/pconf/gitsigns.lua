54966ef355c6756cb776ddf18c5985e154671d6f
kapiyva <ri9.py1201@gmail.com>
2023-09-25 23:59:02 +0900

    fix diffthis keymap

--- lua/pconf/gitsigns.lua
+++ lua/pconf/gitsigns.lua
@@ -30,7 +30,7 @@ require('gitsigns').setup{
     map('n', '<leader>gph', gs.preview_hunk)
     map('n', '<leader>hb', function() gs.blame_line{full=true} end)
     map('n', '<leader>tb', gs.toggle_current_line_blame)
-    map('n', '<leader>hd', gs.diffthis)
+    map('n', '<leader>gd', gs.diffthis)
     map('n', '<leader>hD', function() gs.diffthis('~') end)
     map('n', '<leader>td', gs.toggle_deleted)
 
