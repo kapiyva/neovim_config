9bd2307986de58305ef97220f9cbaff5a728d63d
kapiyva <ri9.py1201@gmail.com>
2023-11-27 12:47:28 +0900

    fix indent

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,115 +1,115 @@
 return {
-    -- lsp管理
-    {
-      "neoclide/coc.nvim",
-      branch = "release",
-      event = "UIEnter",
-      config = function ()
-        require("pconf.coc")
-      end,
-    },
+  -- lsp管理
+  {
+    "neoclide/coc.nvim",
+    branch = "release",
+    event = "UIEnter",
+    config = function ()
+      require("pconf.coc")
+    end,
+  },
 
-    -- ファジーファインダー
-    {
-      "nvim-telescope/telescope.nvim",
-      dependencies = {"nvim-lua/plenary.nvim"},
-      keys = { "<leader>f" },
-      config = function()
-        require("pconf.telescope")
-      end,
-    },
-    { "fannheyward/telescope-coc.nvim" },
-    { "nvim-telescope/telescope-media-files.nvim" },
+  -- ファジーファインダー
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {"nvim-lua/plenary.nvim"},
+    keys = { "<leader>f" },
+    config = function()
+      require("pconf.telescope")
+    end,
+  },
+  { "fannheyward/telescope-coc.nvim" },
+  { "nvim-telescope/telescope-media-files.nvim" },
 
-    -- ファイルツリー
-    {
-      "nvim-treesitter/nvim-treesitter",
-      lazy = true,
-    },
+  -- ファイルツリー
+  {
+    "nvim-treesitter/nvim-treesitter",
+    lazy = true,
+  },
 
-    -- ファイルエクスプローラー
-    {
-      "nvim-tree/nvim-tree.lua",
-      dependencies = { "nvim-tree/nvim-web-devicons" },
-      keys = { "<leader>e" },
-      config = function ()
-        require("pconf.nvim-tree")
-      end,
-    },
+  -- ファイルエクスプローラー
+  {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    keys = { "<leader>e" },
+    config = function ()
+      require("pconf.nvim-tree")
+    end,
+  },
 
-    -- theme
-    {
-      "folke/tokyonight.nvim",
-      config = function ()
-        require("pconf.tokyonight")
-      end,
-    },
+  -- theme
+  {
+    "folke/tokyonight.nvim",
+    config = function ()
+      require("pconf.tokyonight")
+    end,
+  },
 
-    -- ステータスバー装飾
-    {
-      "nvim-lualine/lualine.nvim",
-      dependencies = { "nvim-tree/nvim-web-devicons" },
-      event = "UIEnter",
-      config = function ()
-        require("pconf.lualine")
-      end,
-    },
+  -- ステータスバー装飾
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    event = "UIEnter",
+    config = function ()
+      require("pconf.lualine")
+    end,
+  },
 
-    -- スクロールバー関連
-    {
-      "petertriho/nvim-scrollbar",
-      config = function ()
-        require("pconf.scrollbar")
-      end,
-    },
-    {-- Gitステータスをスクロールバーに表示
-      "lewis6991/gitsigns.nvim",
-      event = "UIEnter",
-      config = function ()
-        require("pconf.gitsigns")
-      end,
-    },
+  -- スクロールバー関連
+  {
+    "petertriho/nvim-scrollbar",
+    config = function ()
+      require("pconf.scrollbar")
+    end,
+  },
+  {-- Gitステータスをスクロールバーに表示
+    "lewis6991/gitsigns.nvim",
+    event = "UIEnter",
+    config = function ()
+      require("pconf.gitsigns")
+    end,
+  },
 
-    -- rust setup
-    {
-      "rust-lang/rust.vim",
-      ft = "rust",
-    },
+  -- rust setup
+  {
+    "rust-lang/rust.vim",
+    ft = "rust",
+  },
 
-    -- カーソルジャンプ
-    { 
-      "phaazon/hop.nvim",
-      config = function()
-        require("pconf.hop")
-      end
-    },
+  -- カーソルジャンプ
+  { 
+    "phaazon/hop.nvim",
+    config = function()
+      require("pconf.hop")
+    end
+  },
 
-    -- add/delete/change surrounding pairs
-    {
-      "kylechui/nvim-surround",
-      config = function()
-          require("nvim-surround").setup({})
-      end
-    },
-    -- copilot
-    {"github/copilot.vim",
-      keys = { "<leader>" },
-    },
-    
-    -- commentary
-    {"tpope/vim-commentary",
-      keys = { "v" },
-    },
+  -- add/delete/change surrounding pairs
+  {
+    "kylechui/nvim-surround",
+    config = function()
+        require("nvim-surround").setup({})
+    end
+  },
+  -- copilot
+  {"github/copilot.vim",
+    keys = { "<leader>" },
+  },
+  
+  -- commentary
+  {"tpope/vim-commentary",
+    keys = { "v" },
+  },
 
-    -- window resize
-    {"simeji/winresizer"},
+  -- window resize
+  {"simeji/winresizer"},
 
-    -- indent line
-    { "lukas-reineke/indent-blankline.nvim",
-      main = "ibl",
-      config = function()
-        require('ibl').setup()
-      end
-    }
+  -- indent line
+  { "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    config = function()
+      require('ibl').setup()
+    end
+  }
 }
 
